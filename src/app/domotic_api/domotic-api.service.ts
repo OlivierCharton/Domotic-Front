249e151e45f1e45279c07fdd3db841d362d32082
Olivier <olivier.charton@live.fr>
2017-04-03 23:51:08 +0200

    [Service] - Récupération de la liste de toutes les pièces + Allumage / Exctinction

--- src/app/domotic_api/domotic-api.service.ts
+++ src/app/domotic_api/domotic-api.service.ts
@@ -1,19 +1,74 @@
+import { Room } from './../model/room';
 import { Injectable } from '@angular/core';
-import { Http }       from '@angular/http';
-import { Observable }     from 'rxjs/Observable';
+import { Http, Response } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 import { AppConfig } from '../app.config';
 
 @Injectable()
 export class DomoticApiService {
-  constructor(private http: Http, private config: AppConfig) {}
+
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  private options = new RequestOptions({ headers: this.headers });
+
+  constructor(private http: Http, private config: AppConfig) { }
+
   test(): Promise<string> {
-    return Promise.resolve(this.config.getConfig('host'));
+    return Promise.resolve(this.getApiUrl());
   }
-  // search(term: string): Observable<Hero[]> {
-  //   return this.http
-  //              .get(`app/heroes/?name=${term}`)
-  //              .map(response => response.json().data as Hero[]);
-  // }
+
+  getInformations(): Observable<Room[]> {
+    let url = this.getApiUrl() + `/informations`;
+    console.log('Appel à l\'url : ' + url);
+
+    return this.http.get(url, this.options)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  turnRoomOn(room: string): Observable<string> {
+    let url = this.getApiUrl() + `/turn_on/` + room;
+    console.log('Appel à l\'url : ' + url);
+
+    return this.http.get(url, this.options)
+      .catch(this.handleError);
+  }
+
+  turnRoomOff(room: string): Observable<string> {
+    let url = this.getApiUrl() + `/turn_off/` + room;
+    console.log('Appel à l\'url  : ' + url);
+
+    return this.http.get(url, this.options)
+      .catch(this.handleError);
+  }
+
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body || {};
+  }
+
+  private handleError(error: Response | any) {
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
+  private getApiUrl() {
+    return this.config.getConfig('host');
+  }
+
 }
+
--- src/app/front_page/front-page.component.ts
+++ src/app/front_page/front-page.component.ts
@@ -1,20 +1,43 @@
+import { Room } from './../model/room';
 import { DomoticApiService } from './../domotic_api/domotic-api.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'front-page',
-  template: 'Host : {{hostName}}'
+  template: `Host : {{hostName}}
+  <br>
+  <div *ngFor="let room of rooms">
+  <button (click)="turnRoomOn(room.name);">Allumer le/la {{room.name}}</button>
+  <button (click)="turnRoomOff(room.name);">Eteindre le/la {{room.name}}</button>
+  </div>`
 })
 
 export class FrontPageComponent implements OnInit {
   hostName: string;
+  name: string;
+  rooms: Room[];
   constructor(private domoticApiService: DomoticApiService) { }
 
   getHostName(): void {
     this.domoticApiService.test().then(hostName => this.hostName = hostName);
   }
 
+  turnRoomOn(room: string): void {
+    this.domoticApiService.turnRoomOn(room).subscribe();
+  }
+
+  turnRoomOff(room: string): void {
+    this.domoticApiService.turnRoomOff(room).subscribe();
+  }
+
+  getInformations(): void {
+    this.domoticApiService.getInformations().subscribe(data => this.rooms = data);
+  }
+
+
   ngOnInit() {
     this.getHostName();
+    this.name = 'Salon';
+    this.getInformations();
   }
 }
