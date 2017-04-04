445bcf285c69f8f82855e3ae3f919687e7a41631
Olivier <olivier.charton.pro@com.com>
2017-04-04 20:21:45 +0200

    [Core] - Suppression de fichiers de conf et protection du contenu de la conf

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,75 +5,53 @@ import { Observable } from 'rxjs/Rx';
 @Injectable()
 export class AppConfig {
 
-    private config: Object = null;
-    private env:    Object = null;
+  private config: Object = null;
+  private env: Object = null;
 
-    constructor(private http: Http) {
+  constructor(private http: Http) {
 
-    }
+  }
 
-    /**
-     * Use to get the data found in the second file (config file)
-     */
-    public getConfig(key: any) {
-        return this.config[key];
-    }
+  /**
+   * Use to get the data found in the second file (config file)
+   */
+  public getConfig(key: any) {
+    return this.config[key];
+  }
 
-    /**
-     * Use to get the data found in the first file (env file)
-     */
-    public getEnv(key: any) {
-        return this.env[key];
-    }
+  /**
+   * Use to get the data found in the first file (env file)
+   */
+  public getEnv(key: any) {
+    return this.env[key];
+  }
 
-    /**
-     * This method:
-     *   a) Loads "env.json" to get the current working environment (e.g.: 'production', 'development')
-     *   b) Loads "config.[env].json" to get all env's variables (e.g.: 'config.development.json')
-     */
-    public load() {
-        return new Promise((resolve, reject) => {
-            this.http.get('./env.json').map( res => res.json() ).catch((error: any): any => {
-                console.log('Configuration file "env.json" could not be read');
-                resolve(true);
-                return Observable.throw(error.json().error || 'Server error');
-            }).subscribe( (envResponse) => {
-                this.env = envResponse;
-                let request: any = null;
+  /**
+   * This method:
+   *   a) Loads "env.json" to get the current working environment (e.g.: 'production', 'development')
+   *   b) Loads "config.[env].json" to get all env's variables (e.g.: 'config.development.json')
+   */
+  public load() {
+    return new Promise((resolve, reject) => {
+      let request: any = null;
+      request = this.http.get('./config.json');
 
-                switch (this.getEnv('env')) {
-                    case 'production': {
-                        request = this.http.get('./config.' + this.getEnv('env') + '.json');
-                    } break;
-
-                    case 'development': {
-                        request = this.http.get('./config.' + this.getEnv('env') + '.json');
-                    } break;
-
-                    case 'default': {
-                        console.error('Environment file is not set or invalid');
-                        resolve(true);
-                    } break;
-                }
-
-                if (request) {
-                    request
-                        .map( (res: any) => res.json() )
-                        .catch((error: any) => {
-                            console.error('Error reading ' + this.getEnv('env') + ' configuration file');
-                            resolve(error);
-                            return Observable.throw(error.json().error || 'Server error');
-                        })
-                        .subscribe((responseData: any) => {
-                            this.config = responseData;
-                            resolve(true);
-                        });
-                } else {
-                    console.error('Env config file "env.json" is not valid');
-                    resolve(true);
-                }
-            });
-
-        });
-    }
+      if (request) {
+        request
+          .map((res: any) => res.json())
+          .catch((error: any) => {
+            console.error('Error reading configuration file !');
+            resolve(error);
+            return Observable.throw(error.json().error || 'Server error');
+          })
+          .subscribe((responseData: any) => {
+            this.config = responseData;
+            resolve(true);
+          });
+      } else {
+        console.error('Config file "config.json" is not valid');
+        resolve(true);
+      }
+    });
+  }
 }
